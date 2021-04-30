f0844aba206e47ecfed034b73e1f5c8095bf950f
abhishekkumar <abhishek.kp22@gmail.com>
2021-04-30 16:17:55 +0530

    Initial commit

---
+++ lib/userRagistration/uploadAndStore.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+
+class upload extends StatefulWidget {
+  @override
+  _uploadState createState() => _uploadState();
+}
+
+class _uploadState extends State<upload> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          ""
+        ),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
